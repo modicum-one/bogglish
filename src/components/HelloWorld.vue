a5e33c33c186c56d4fc61a8c70d2bb0e93593c29
depstei2 <depstei2@umd.edu>
2020-11-24 10:51:26 -0500

    Pushing changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,91 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1 v-bind:class="{ blinking: timerSeconds==0 }">Bogglish {{timerDisplay}}</h1>
+    <button role="button" v-on:click="shake">Shake</button>
+    <div class="board">
+    <table>
+      <tr v-for="(row, indexY) in board" :key="indexY">
+        <td v-for="(col, indexX) in row" :key="indexX">
+          <div class="cube">{{col}}</div>
+        </td>
+      </tr>
+    </table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  created: function () {
+    this.shake();
+    setInterval(()=>{
+      this.timerTick();
+    },1000)
+  },
+  methods: {
+    shake: function () {
+      // console.log("shake it up"+this.board);
+      shuffle(this.letterCubes);
+      this.board=[];
+      for(let i = 0; i<4; i++){
+        let row =[];
+        for(let j=0;j<4; j++){
+          row.push(this.letterCubes[(i*4)+j][0]);
+        }
+        this.board.push(row);
+      }
+      this.timerSeconds=180;
+      new Audio('./Shake.m4a').play();
+    },
+    timerTick: function () {
+      if(this.timerSeconds > 0) {
+        this.timerSeconds--;
+        let date = new Date(0);
+        date.setSeconds(this.timerSeconds); // specify value for SECONDS here
+        this.timerDisplay = date.toISOString().substr(11, 8);
+        if (this.timerSeconds == 0){
+          new Audio('./awooga-awooga-awooga.mp3').play();
+        }
+      }
+    }
+  },
+  data: function() {return {
+    timeouts:{},
+    timerSeconds:0,
+    timerDisplay:'',
+    letterCubes: [
+            ['I','S','P','E','N','H'], //0
+            ['F','O','X','I','B','R'], //1
+            ['B','I','T','Y','A','L'], //2
+            ['D','U','T','N','O','K'], //3
+            ['C','A','L','S','E','R'], //4
+            ['V','E','N','Z','A','D'], //5
+            ['G','E','K','Y','U','L'], //6
+            ['P','U','T','L','E','S'], //7
+            ['S','O','D','W','E','N'], //8
+            ['B','O','M','J','A','Qu'], //9
+            ['V','E','G','N','I','T'], //10
+            ['L','U','R','W','I','G'], //11
+            ['M','E','C','P','A','D'], //12
+            ['I','E','H','Y','E','F'], //13
+            ['C','O','I','T','A','A'], //14
+            ['S','A','M','R','O','H'], //15
+    ],
+    board:[['','','',''],
+           ['','','',''],
+           ['','','',''],
+           ['','','','']]
+  }
+  }
+}
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+    if(Array.isArray(array[i])){
+      shuffle(array[i])
+    }
   }
 }
 </script>
@@ -55,4 +106,30 @@ li {
 a {
   color: #42b983;
 }
+table{
+  margin-left:auto;
+  margin-right:auto;
+}
+div .board{
+
+}
+  div .cube{
+    font-size: 5rem;
+    box-shadow: 5px 5px #888888;
+    border:1pt solid black;
+    margin:6px;
+    width:6rem;
+  }
+
+.blinking{
+  animation:blinkingText 1.2s infinite;
+}
+@keyframes blinkingText{
+  0%{     color: #F00;    }
+  49%{    color: #F00; }
+  60%{    color: transparent; }
+  99%{    color: transparent;  }
+  100%{   color: #F00;    }
+}
+
 </style>
